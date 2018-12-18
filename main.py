9cd680166555245742218927bf8716f501491141
wenwei202 <weiwen.web@gmail.com>
2018-12-18 10:50:36 -0800

    plotting and saving and logging

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 '''Train CIFAR10 with PyTorch.'''
 from __future__ import print_function
 
+import matplotlib
+matplotlib.use("pdf")
+import matplotlib.pyplot as plt
+import logging
+from datetime import datetime
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -12,6 +18,7 @@ import torchvision.transforms as transforms
 
 import os
 import argparse
+import numpy as np
 
 from models import *
 from utils import progress_bar
@@ -20,6 +27,7 @@ from utils import progress_bar
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--optimizer', default='sgd', type=str, help='sgd variants (sgd, adam, amsgrad)')
+
 parser.add_argument('--epochs', default=300, type=int, help='the number of epochs')
 parser.add_argument('--batch-size', default=128, type=int, help='batch size')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
@@ -35,8 +43,19 @@ device = 'cuda' # if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
+save_path = os.path.join('./results', datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+else:
+    raise OSError('Directory {%s} exists. Use a new one.' % save_path)
+logging.basicConfig(filename=os.path.join(save_path, 'log.txt'), level=logging.INFO)
+logger = logging.getLogger('main')
+logger.addHandler(logging.StreamHandler())
+logger.info("Saving to %s", save_path)
+logger.info("Running arguments: %s", args)
+
 # Data
-print('==> Preparing data..')
+logger.info('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
@@ -58,7 +77,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
-print('==> Building model..')
+logger.info('==> Building model..')
 # net = VGG('VGG19')
 net = ResNet18()
 # net = PreActResNet18()
@@ -77,7 +96,7 @@ if device == 'cuda':
 
 if args.resume:
     # Load checkpoint.
-    print('==> Resuming from checkpoint..')
+    logger.info('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
     checkpoint = torch.load('./checkpoint/ckpt.t7')
     net.load_state_dict(checkpoint['net'])
@@ -96,7 +115,7 @@ else:
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    logger.info('\nEpoch: %d (train)' % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -113,11 +132,13 @@ def train(epoch):
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
-
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if 0 == batch_idx % 100 or batch_idx == len(trainloader) - 1:
+            logger.info('(%d/%d) ==> Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                % (batch_idx+1, len(trainloader), train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    return train_loss / len(trainloader), 100.*correct/total
 
 def test(epoch):
+    logger.info('Epoch: %d (test)' % epoch)
     global best_acc
     net.eval()
     test_loss = 0
@@ -133,26 +154,52 @@ def test(epoch):
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if 0 == batch_idx % 100 or batch_idx == len(testloader) - 1:
+                logger.info('(%d/%d) ==> Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (batch_idx+1, len(testloader), test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
-        print('Saving..')
+        logger.info('Saving best @ {epoch %d}..' % epoch)
         state = {
             'net': net.state_dict(),
             'acc': acc,
             'epoch': epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.t7')
+        torch.save(state, os.path.join(save_path, 'best_ckpt.t7'))
         best_acc = acc
 
+    return test_loss / len(testloader), acc
+
+# main func
+# epoch, train loss, train accu, test loss, test accu
+curves = np.zeros((args.epochs, 5))
+clr1 = (0.6, 0., 0.)
+clr2 = (0.42, 0.56, 0.14)
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.set_xlabel('epoch')
+ax1.set_ylabel('Loss', color=clr1)
+ax1.tick_params(axis='y', colors=clr1)
+ax2.set_ylabel('Accuracy (%)', color=clr2)
+ax2.tick_params(axis='y', colors=clr2)
 
 for epoch in range(start_epoch, start_epoch+args.epochs):
     adjust_learning_rate(optimizer, epoch, args)
-    train(epoch)
-    test(epoch)
+    curves[epoch, 0] = epoch
+    curves[epoch, 1], curves[epoch, 2] = train(epoch)
+    curves[epoch, 3], curves[epoch, 4] = test(epoch)
+
+    # plotting
+    ax1.semilogy(curves[:epoch+1, 0], curves[:epoch+1, 1], '--', color=clr1, mfc=clr1, markersize=2)
+    ax1.semilogy(curves[:epoch+1, 0], curves[:epoch+1, 3], '-', color=clr1, mfc=clr1, markersize=2)
+    ax2.plot(curves[:epoch+1, 0], curves[:epoch+1, 2], '--', color=clr2, mfc=clr2, markersize=2)
+    ax2.plot(curves[:epoch+1, 0], curves[:epoch+1, 4], '-', color=clr2, mfc=clr2, markersize=2)
+    ax1.legend(('Train loss', 'Val loss'), loc='lower right')
+    ax2.legend(('Train accuracy', 'Val accuracy'), loc='upper left')
+
+    #        ax1.grid(b=True, which='both')
+    plt.savefig(os.path.join(save_path, 'curves.pdf'))
+
+logger.info('Done!')
