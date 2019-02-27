4219447be4aa55d658430e59d9448f7188461598
wenwei202 <weiwen.web@gmail.com>
2019-02-27 08:13:24 -0800

    more nets in profiling

--- profile.py
+++ profile.py
@@ -26,7 +26,23 @@ nets = [
     ('CifarResNetBasic', [18, 18, 18], 93.79),
     ('CifarResNetBasic', [24, 24, 24], 94.26),
     ('CifarResNetBasic', [48, 48, 48], 94.54),
-    # growing
+    # growing with gaussian
+#    ('CifarResNetBasic', [5, 5, 4], 92.68),
+    ('CifarResNetBasic', [4, 6, 3], 93.11),
+    ('CifarResNetBasic', [10, 10, 10], 93.34),
+    ('CifarResNetBasic', [11, 8, 11], 93.41),
+#    ('CifarResNetBasic', [9, 16, 16], 93.34),
+    ('CifarResNetBasic', [24, 23, 23], 93.48),
+    ('CifarResNetBasic', [33, 32, 32], 94.15),
+    ('CifarResNetBasic', [92, 91, 91], 94.41),
+
+    # growing with zero
+#    ('CifarResNetBasic', [6, 3, 5], 92.28),
+#    ('CifarResNetBasic', [3, 3, 6], 92.56),
+    ('CifarResNetBasic', [5, 5, 4], 92.99),
+    ('CifarResNetBasic', [22, 6, 6], 93.18),
+    ('CifarResNetBasic', [49, 18, 18], 93.33),
+    ('CifarResNetBasic', [32, 31, 31], 93.86),
     ('CifarResNetBasic', [42, 42, 41], 94.31),
 
 ]
