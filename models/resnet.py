2beb505da903dfde1b50ed41e175eb5022492a99
wenwei202 <weiwen.web@gmail.com>
2019-03-12 18:01:31 -0700

    PlainNoBNNet

--- models/resnet.py
+++ models/resnet.py
@@ -49,6 +49,16 @@ class PlainBlock(nn.Module):
         out = F.relu(self.bn1(self.conv1(x)))
         return out
 
+class PlainNoBNBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PlainNoBNBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        return out
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -104,12 +114,15 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, image_channels=3):
+    def __init__(self, block, num_blocks, num_classes=10, image_channels=3, batchnorm=True):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        if batchnorm:
+            self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(64)
+        else:
+            self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, stride=1, padding=1, bias=True)
+            self.bn1 = nn.Sequential()
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -178,6 +191,11 @@ def PlainNet(num_blocks, num_classes=10, image_channels=3):
     assert len(num_blocks) == 4, "4 blocks are needed, but %d is found." % len(num_blocks)
     # ResNet is NOT a ResNet, it's just a building func
     return ResNet(PlainBlock, num_blocks, num_classes=num_classes, image_channels=image_channels)
+
+def PlainNoBNNet(num_blocks, num_classes=10, image_channels=3):
+    assert len(num_blocks) == 4, "4 blocks are needed, but %d is found." % len(num_blocks)
+    # ResNet is NOT a ResNet, it's just a building func
+    return ResNet(PlainNoBNBlock, num_blocks, num_classes=num_classes, image_channels=image_channels, batchnorm=False)
 
 def ResNetBasic(num_blocks, num_classes=10, image_channels=3):
     assert len(num_blocks) == 4, "4 blocks are needed, but %d is found." % len(num_blocks)
